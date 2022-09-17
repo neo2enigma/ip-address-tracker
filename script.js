98b68d511640fee4e6310641d7b150f24a2466ea
Kamran <neo2enigma@gmail.com>
2022-09-17 14:02:51 +0430

    javascript map done

--- script.js
+++ script.js
@@ -1,18 +1,54 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
-fetch('https://api.db-ip.com/v2/free/self').then(data => console.log(data));
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+let map;
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
-
-// new implementation
+const apiKey = 'at_BDVP1HqV2uRogHt1o4TB0m52iGqe8';
+const url = 'https://geo.ipify.org/api/v2/country,city';
 
 
-// let myIp;
-// $.getJSON('https://api.db-ip.com/v2/free/self', function (data) {
-//     myIp = JSON.stringify(data, null, 2);
-//     console.log(myIp);
-// });
+function getGeo(ipAddress = '') {
+    return Promise.resolve($.ajax({
+        url,
+        data: { apiKey, ipAddress },
+        success: function (data) {
+            return JSON.stringify(data, "", 2);
+        }
+    }));
+
+}
+
+function extractData(result) {
+    const { ip, location, isp } = result;
+    return { ip, location: `${location.region} ${location.city}`, timezone: location.timezone, isp, latlng: [location.lat, location.lng] };
+}
+
+function painMap(latlng) {
+    map = L.map('map').setView(latlng, 16);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    // L.marker(latlng).addTo(map)
+    //     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    //     .openPopup();
+
+}
+
+function showPop(result) {
+    L.marker(result.latlng).addTo(map)
+        .bindPopup(`IP Address: ${result.ip}`)
+        .openPopup();
+}
+
+
+
+
+
+window.addEventListener('load', (e) => {
+    getGeo().then(data => {
+        // console.log(data);
+        const result = extractData(data);
+        // console.log(result.latlng);
+        painMap(result.latlng);
+        showPop(result);
+    }).catch(err => console.log(err));
+});
