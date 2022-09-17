174691e98cdc9c5bad472bd045703358e23c102c
Kamran <neo2enigma@gmail.com>
2022-09-17 17:19:12 +0430

    javascript map done

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 let map;
 
+
 const apiKey = 'at_BDVP1HqV2uRogHt1o4TB0m52iGqe8';
 const url = 'https://geo.ipify.org/api/v2/country,city';
+
+
+
+function getValues() {
+    let params;
+    // const keys = Object.keys[localStorage];
+    // console.log(keys);
+    // for (let i = 0; i < keys.length; i++) {
+    //     console.log(localStorage.getItem(keys[i]));
+    //     params.push(localStorage.key[i]);
+    // }
+    params = { ...localStorage };
+    // console.log(params);
+    return params;
+}
+
+function setValues(params) {
+    for (const key in params) {
+        localStorage.setItem(key, params[key]);
+
+    }
+}
+
 
 
 function getGeo(ipAddress = '') {
@@ -15,40 +39,49 @@ function getGeo(ipAddress = '') {
 
 }
 
-function extractData(result) {
-    const { ip, location, isp } = result;
-    return { ip, location: `${location.region} ${location.city}`, timezone: location.timezone, isp, latlng: [location.lat, location.lng] };
+function extractData(data) {
+    let result = { ip: data.ip, ...data.location, isp: data.isp };
+    return result;
 }
 
-function painMap(latlng) {
-    map = L.map('map').setView(latlng, 16);
 
+function painMap(lat, lng) {
+
+    map = L.map('map').setView([lat, lng], 16);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    // L.marker(latlng).addTo(map)
-    //     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    //     .openPopup();
 
 }
 
-function showPop(result) {
-    L.marker(result.latlng).addTo(map)
-        .bindPopup(`IP Address: ${result.ip}`)
+function showPop(lat, lng, ip) {
+    L.marker([lat, lng]).addTo(map)
+        .bindPopup(`IP Address: ${ip}`)
         .openPopup();
 }
 
 
 
+async function getLocation() {
+    try {
+        const ip = localStorage.getItem('ip') ? localStorage.getItem('ip') : '';
+        let result;
+        if (ip === '') {
+            const data = await getGeo();
+            result = extractData(data);
+            setValues(result);
+        } else {
+            result = getValues();
+        }
+        return result;
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 
-window.addEventListener('load', (e) => {
-    getGeo().then(data => {
-        // console.log(data);
-        const result = extractData(data);
-        // console.log(result.latlng);
-        painMap(result.latlng);
-        showPop(result);
-    }).catch(err => console.log(err));
-});
+getLocation().then(result => {
+    painMap(result['lat'], result['lng']);
+    showPop(result['lat'], result['lng'], result['ip']);
+}).catch(err => console.log(err));
