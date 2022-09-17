c3bfca63f94a428bcde6f62d39d374ab4117e643
Kamran <neo2enigma@gmail.com>
2022-09-17 11:23:57 +0430

    js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var map = L.map('map').setView([51.505, -0.09], 13);
-
+fetch('https://api.db-ip.com/v2/free/self').then(data => console.log(data));
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
@@ -7,3 +7,9 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 L.marker([51.5, -0.09]).addTo(map)
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
     .openPopup();
+
+// let myIp;
+// $.getJSON('https://api.db-ip.com/v2/free/self', function (data) {
+//     myIp = JSON.stringify(data, null, 2);
+//     console.log(myIp);
+// });
